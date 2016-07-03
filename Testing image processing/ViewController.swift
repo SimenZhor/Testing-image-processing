16f70feeb662dc40695ef5402c28ede385744ed3
SimenZhor <simen_sor91@hotmail.com>
2016-07-03 14:55:22 -0700

    Initial Commit

---
+++ Testing image processing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Testing image processing
+//
+//  Created by Simen E. Sørensen on 03/07/16.
+//  Copyright © 2016 Simen E. Sørensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
